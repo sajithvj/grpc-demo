151b8c1502fd5eb558f4af68ab95537741611d16
sajithvj <sajithvj@gmail.com>
2026-09-16 16:20:24 +0530

    Changes of secrtes

--- src/test/java/com/example/grpcdemo/Consumer.java
+++ src/test/java/com/example/grpcdemo/Consumer.java
@@ -11,15 +11,15 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 public final class Consumer {
   public static void main(String[] args) {
     Properties props = new Properties();
-    props.setProperty("bootstrap.servers", "kafka-3d3dad9a-sajithjayaraman74-192c.l.aivencloud.com:13703");
+    props.setProperty("bootstrap.servers", "");
     props.setProperty("security.protocol", "SASL_SSL");
     // Note: When running in an IDE, you may need to specify
     // the full path to certificate files
     // Example: props.setProperty("ssl.truststore.location", "/full/path/to/ca.pem");
-    props.setProperty("ssl.truststore.location", "D:\\grpc-demo\\src\\main\\resources\\client-certs\\ca.pem");
+    props.setProperty("ssl.truststore.location", "");
     props.setProperty("ssl.truststore.type", "PEM");
     props.setProperty("sasl.mechanism", "SCRAM-SHA-256");
-    props.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"avnadmin\" password=\"AVNS_v4EywHz-wycHum3wwo8\";");
+    props.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=password=;");
     props.setProperty("group.id", "topic_1-consumer");
     props.setProperty("auto.offset.reset", "latest");
     props.setProperty("key.deserializer", StringDeserializer.class.getName());
